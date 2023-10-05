1b04ade2738bd46ce9efd2c78730d35f5a2bc973
Roesn <Bubwub04@icloud.com>
2023-10-05 05:10:58 -0400

    finsihing otuches on button styling

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// hooks 
+questionPageButton = document.getElementById("questions-button");
+highscorePageButton = document.getElementById("high-score-button");
+
+// event listeners
+questionPageButton.addEventListener("click", e => {
+    // remove hidden class from questions div
+    // hide rest of pages
+});
+
+highscorePageButton.addEventListener("click", e => {
+    // remove hidden class from highscore div
+    // hide rest of pages
+});
